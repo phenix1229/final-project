fd6d71a9f1cdd8a209b13c257674ad01ee339b35
phenix1229 <paul.garay@codeimmersives.com>
2019-12-18 21:17:15 -0500

    added rounding to 2 decimal places, changed wording from debit/credit to deposit/withdrawal, altered display of menu options

--- debtCred.js
+++ debtCred.js
@@ -10,9 +10,9 @@ const submitAction = document.querySelector('#submitAction');
 
 const docVal = () => {
     if(debtOrCred.value === '1'){
-        return 'Debit';
+        return 'Deposit';
     } else {
-        return 'Credit';
+        return 'Withdrawal';
     }
 }
 
@@ -25,42 +25,44 @@ const acctVal = () => {
 }
 
 const calcBalAfterAction = () => {
-    if (debtOrCred.value === '1' && acctChoice.value === '1'){
-        dataObj.checkingBalance = Number(dataObj.checkingBalance) - Number(dollarAmount.value);
+    const adjDolAmt = Math.round(dollarAmount.value * 100) / 100;
+    if (debtOrCred.value === '2' && acctChoice.value === '1'){
+        dataObj.checkingBalance = Number(dataObj.checkingBalance) - Number(adjDolAmt);
+    } else if (debtOrCred.value === '2' && acctChoice.value === '2'){
+        dataObj.savingsBalance = Number(dataObj.savingsBalance) - Number(adjDolAmt);
+    } else if (debtOrCred.value === '1' && acctChoice.value === '1'){
+        dataObj.checkingBalance = Number(dataObj.checkingBalance) + Number(adjDolAmt);
     } else if (debtOrCred.value === '1' && acctChoice.value === '2'){
-        dataObj.savingsBalance = Number(dataObj.savingsBalance) - Number(dollarAmount.value);
-    } else if (debtOrCred.value === '2' && acctChoice.value === '1'){
-        dataObj.checkingBalance = Number(Number(dataObj.checkingBalance) + Number(dollarAmount.value));
-    } else if (debtOrCred.value === '2' && acctChoice === '2'){
-        dataObj.savingsBalance = Number(dataObj.savingsBalance) + Number(dollarAmount.value);
+        dataObj.savingsBalance = Number(dataObj.savingsBalance) + Number(adjDolAmt);
     }
 }
 
 document.querySelector('#submitAction').addEventListener('click', function(){
     if (checkForNumbers(dollarAmount.value) || !dollarAmount){
-        alert('Please enter dollar amount as a number.');
+        alert('Please enter dollar amount as a number.\nExample: 1.00');
         dollarAmount.value = '';
     } else {
-        if (dollarAmount && debtOrCred.value === '1'){
+        const adjDolAmt = Math.round(dollarAmount.value * 100) / 100;
+        if (dollarAmount && debtOrCred.value === '2'){
             if (acctChoice.value === '1'){
                 dataObj.cActDate.push(today());
                 dataObj.cActDesc.push(`${docVal()}. ${description.value}.`);
-                dataObj.cActAmt.push(`-${dollarAmount.value}`);
+                dataObj.cActAmt.push(`-${adjDolAmt}`);
             } else if (acctChoice.value === '2'){
                 dataObj.sActDate.push(today());
                 dataObj.sActDesc.push(`${docVal()}. ${description.value}.`);
-                dataObj.sActAmt.push(`-${dollarAmount.value}`);
+                dataObj.sActAmt.push(`-${adjDolAmt}`);
             }
         }
-        if(dollarAmount && debtOrCred.value === '2'){
+        if(dollarAmount && debtOrCred.value === '1'){
             if (acctChoice.value === '1'){
                 dataObj.cActDate.push(today());
                 dataObj.cActDesc.push(`${docVal()}. ${description.value}.`);
-                dataObj.cActAmt.push(`${dollarAmount.value}`);
+                dataObj.cActAmt.push(`${adjDolAmt}`);
             } else if (acctChoice.value === '2'){
                 dataObj.sActDate.push(today());
                 dataObj.sActDesc.push(`${docVal()}. ${description.value}.`);
-                dataObj.sActAmt.push(`${dollarAmount.value}`);
+                dataObj.sActAmt.push(`${adjDolAmt}`);
             }
         }
     }
--- transfer.js
+++ transfer.js
@@ -21,12 +21,13 @@ const ttVal = () => {
 }
 
 const calcBalAfterTrans = () => {
+    const transDolAmt = Math.round(transAmount.value * 100) / 100;
     if (transFrom.value === '1' && transTo.value === '2'){
-        dataObj.checkingBalance = Number(dataObj.checkingBalance) - Number(transAmount.value);
-        dataObj.savingsBalance = Number(dataObj.savingsBalance) + Number(transAmount.value);
+        dataObj.checkingBalance = Number(dataObj.checkingBalance) - Number(transDolAmt);
+        dataObj.savingsBalance = Number(dataObj.savingsBalance) + Number(transDolAmt);
     } else if (transFrom.value === '2' && transTo.value === '1'){
-        dataObj.savingsBalance = Number(dataObj.savingsBalance) - Number(transAmount.value);
-        dataObj.checkingBalance = Number(dataObj.checkingBalance) + Number(transAmount.value);
+        dataObj.savingsBalance = Number(dataObj.savingsBalance) - Number(transDolAmt);
+        dataObj.checkingBalance = Number(dataObj.checkingBalance) + Number(transDolAmt);
     }
 }
 
@@ -34,24 +35,26 @@ const calcBalAfterTrans = () => {
 
 document.querySelector('#submitTrans').addEventListener('click', function(){
     if (checkForNumbers(transAmount.value) || !transAmount){
-        alert('Please enter transfer amount as a number.');
+        alert('Please enter transfer amount as a number.\nExample: 1.00');
         transAmount.value = '';
     } else {
+        const transDolAmt = Math.round(transAmount.value * 100) / 100;
         if(transAmount && transFrom.value === '1'){
+
             dataObj.cActDate.push(today());
             dataObj.sActDate.push(today());
-            dataObj.cActDesc.push(`Transfer to ${ttVal()}`);
-            dataObj.sActDesc.push(`Transfer from ${tfVal()}`);
-            dataObj.cActAmt.push(`-${transAmount.value}`);
-            dataObj.sActAmt.push(`${transAmount.value}`);
+            dataObj.cActDesc.push(`Transfer to ${ttVal()}.`);
+            dataObj.sActDesc.push(`Transfer from ${tfVal()}.`);
+            dataObj.cActAmt.push(`-${transDolAmt}`);
+            dataObj.sActAmt.push(`${transDolAmt}`);
         }
         if(transAmount && transFrom.value === '2'){
             dataObj.cActDate.push(today());
             dataObj.sActDate.push(today());
             dataObj.sActDesc.push(`Transfer to ${ttVal()}.`);
             dataObj.cActDesc.push(`Transfer from ${tfVal()}.`);
-            dataObj.sActAmt.push(`-${transAmount.value}`);
-            dataObj.cActAmt.push(`${transAmount.value}`);
+            dataObj.sActAmt.push(`-${transDolAmt}`);
+            dataObj.cActAmt.push(`${transDolAmt}`);
         }
     }
     calcBalAfterTrans();
